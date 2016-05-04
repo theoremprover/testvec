d302f46e77236174f0018ee85ff3f0822b0a6ce6
Robert <no@email.com>
2016-05-04 22:53:18 +0200

    <no message>

--- testvec.hs
+++ testvec.hs
@@ -1,9 +1,17 @@
 module Main where
 
 import Language.C
-import Language.C.System.GCC   -- preprocessor used
+import Language.C.System.GCC   -- Preprocessor
+import Language.C.Analysis.TravMonad
+import Language.C.Analysis.AstAnalysis
+import Language.C.Analysis.SemRep
+import qualified Data.Map as Map
+import Control.Monad
+import System.Environment
 
-main = parseMyFile "printf.i" >>= printMyAST
+main = do
+	[sourcefile] <- getArgs
+	parseMyFile sourcefile >>= analyze
 
 parseMyFile :: FilePath -> IO CTranslUnit
 parseMyFile input_file = do
@@ -12,5 +20,11 @@ parseMyFile input_file = do
 		Left parse_err -> error (show parse_err)
 		Right ast      -> return ast
 
-printMyAST :: CTranslUnit -> IO ()
-printMyAST ctu = (print . pretty) ctu
+analyze :: CTranslUnit -> IO ()
+analyze ctu = do
+	let (Right (globaldecls,[])) = runTrav_ (analyseAST ctu)
+	forM_ (Map.toList $ gObjs globaldecls) $ \ (ident,decl) -> do
+		case decl of
+			FunctionDef (FunDef (VarDecl (VarName ident _) declattrs ty) stmt nodeinfo) -> do
+				putStrLn $ show ident
+			_ -> return ()
