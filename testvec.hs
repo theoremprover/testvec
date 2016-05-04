2a04199eaf40e784ba45c776f6f417cab40f3868
Robert <no.email@given.de>
2016-05-04 20:11:46 +0200

    No message

---
+++ testvec.hs
@@ -0,0 +1,16 @@
+module Main where
+
+import Language.C
+import Language.C.System.GCC   -- preprocessor used
+
+main = parseMyFile "printf.i" >>= printMyAST
+
+parseMyFile :: FilePath -> IO CTranslUnit
+parseMyFile input_file = do
+	parse_result <- parseCFile (newGCC "gcc") Nothing [] input_file
+	case parse_result of
+		Left parse_err -> error (show parse_err)
+		Right ast      -> return ast
+
+printMyAST :: CTranslUnit -> IO ()
+printMyAST ctu = (print . pretty) ctu
