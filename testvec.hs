f3a930ed1e62400c5339ed732cab16e300c32aad
Robert <no.email@given.de>
2016-05-09 09:11:21 +0200

    No Message

--- testvec.hs
+++ testvec.hs
@@ -7,6 +7,7 @@ import Language.C.System.GCC   -- Preprocessor
 import Language.C.Analysis.TravMonad
 import Language.C.Analysis.AstAnalysis
 import Language.C.Analysis.SemRep
+import Language.C.Analysis.DefTable
 import qualified Data.Map as Map
 import Control.Monad
 import System.Environment
@@ -24,6 +25,24 @@ parseMyFile input_file = do
 
 analyze :: CTranslUnit -> IO ()
 analyze (CTranslUnit extdecls nodeinfo) = do
-	forM_ extdecls analyseExt
-	globdecls <- liftM globalDefs getDefTable
-forM_ decls $ \ 
+	forM_ extdecls $ \case
+		CFDefExt fundef -> handleTravError $ analyseFunDef fundef
+		CDeclExt decl -> return Nothing
+		CAsmExt asm _ -> error "Found CAsmExt"
+	getErrors >>= sequence_ . (map print)
+
+	globdefs <- liftM globalDefs getDefTable
+	forM_ (Map.toList $ gObjs globdefs) $ \case
+		(ident,Declaration decl) -> print ident
+		_ -> return ()
+
+{-
+    mapRecoverM_ analyseExt decls
+    -- check we are in global scope afterwards
+    getDefTable >>= \dt -> when (not (inFileScope dt)) $
+        error "Internal Error: Not in filescope after analysis"
+    -- get the global definition table (XXX: remove ?)
+    
+    where
+    mapRecoverM_ f = mapM_ (handleTravError . f)
+-}
