3b50b75941d4365b83754ce0b6e05e1e5e8cc916
Robert <no@email.com>
2016-05-12 18:23:06 +0200

    <no message>

--- testvec.hs
+++ testvec.hs
@@ -24,18 +24,44 @@ parseMyFile input_file = do
 		Left parse_err -> error (show parse_err)
 		Right ast      -> return ast
 
+notImplYet x = error $ show x ++ " not implemented yet"
+
 analyze :: CTranslUnit -> IO ()
 analyze (CTranslUnit extdecls nodeinfo) = do
-	forM_ extdecls $ \case
+	forM_ extdecls $ \ extdecl -> case
 		CDeclExt decl   -> analyzeDecl decl
 		CFDefExt fundef -> analyzeFunDef fundef
-		CAsmExt asm _   -> error "Found CAsmExt"
+		CAsmExt asm _   -> notImplYet extdecl
 
 analyzeDecl (CDecl declspecs diss nodeinfo) = do
 	forM_ diss $ \case
 		_ -> return ()
 
-analyzeFunDef (CFunDef declspecs (CDeclr (Just (Ident name _ _)) _ _ _ _) cdecls stmt nodeinfo) = do
-	print name
+-- Possible InputValues is a list of value ranges
+type InputValues a = Any | Ranges [(a,a)]
+	deriving (Show,Eq)
 
-showBoth x = putStrLn $ printf "%s :\n  %s\n" (show x) (render $ pretty x)
+type InputTypes a = Map.Map Ident (CTypeSpecifier a)
+
+class SymbValueRepr a where
+	
+printIdent (Ident name _ _) = putStrLn name
+
+analyzeFunDef (CFunDef declspecs (CDeclr (Just ident) _ _ _ _) cdecls stmt nodeinfo) = do
+	putStrLn "--------------------------------------"
+	printIdent ident
+	paths <- followStmt inputs stmt
+	print paths
+	where
+	inputs = concatMap extracttype declspecs
+	extracttype declspec = case declspec of
+		CTypeSpec ctypespec -> 
+
+followStmt inputs stmt = case stmt of
+	CExpr (Just cexpr) nodeinfo -> followExpr inputs cexpr
+	_ -> notImplYet stmt
+
+followExpr inputs cexpr = case cexpr of
+	CAssign assignop (CVar ident _) assignedexpr _ -> 
+	_ -> notImplYet cexpr
+
