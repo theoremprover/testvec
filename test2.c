ae1badc318f9add4c6bfa218faec4b71e6cb43a7
Robert <no.email@given.de>
2017-05-10 16:42:34 +0200

    No Message

--- test2.c
+++ test2.c
@@ -2,8 +2,61 @@
 
 int abs(int x)
 {
-	if(x>=0) return x;
-	else return -x;
+	if(x>0) return x;
+	else return 0-x;
+}
+
+// Analyse mode
+int g_ana_abs(int x,int y)
+{
+	// [y=Any,x=2],[y=Any,x=1]
+	int e = y;
+
+	// [e=Any,x=2],[e=Any,x=1]
+	if(x>abs(y))
+	{
+		// [e = Any]
+		e = e * 2;
+	}
+
+	// [e = Any]
+	return e-1;
+	// [erg = Any]
+}
+
+// Analyse mode
+int g_ana(int x,int y)
+{
+	// [y=Any,x=2],[y=Any,x=1]
+	int e = y;
+
+	// [e=Any,x=2],[e=Any,x=1]
+	if(x>1)
+	{
+		// [e = Any]
+		e = e * 2;
+	}
+
+	// [e = Any]
+	return e-1;
+	// [erg = Any]
+}
+
+// Reverse mode
+int g_rev(int x,int y)
+{
+	// [y=3,x=2],[y=6,x=1]
+	int e = y;
+	// [e=3,x=2],[e=6,x=1]
+	if(x>1)
+	{
+		// [e = 3]
+		e = e * 2;
+	}
+
+	// [e = 6]
+	return e-1;
+	// [erg = 5]
 }
 
 int f(int x,int y)
@@ -13,21 +66,15 @@ int f(int x,int y)
 	int f = 1;
 	if(x<0)
 	{
-// vals = [
 		f = -1;
-// vals = [f=Any]
 	}
-// vals = [f=Any]
 
-/*
 	for(int i=1;i<=abs(x);i++)
 	{
 		erg = erg + f*y;
 	}
-*/
 
-	return f /*erg*/;
-// vals = [erg=Any]
+	return erg;
 }
 
 int main()
